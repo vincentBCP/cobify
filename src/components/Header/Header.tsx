305278fffd954db49f387f604dd32e308a91f0ad
Vincent <vincentbenjaminpatoc@gmail.com>
2021-04-05 12:18:04 +0800

    Use Avatar widget in Header component.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-import { createStyles, makeStyles, Theme, withStyles, useTheme } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme, withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import NotificationIcon from '@material-ui/icons/NotificationsNoneOutlined';
 import Badge from '@material-ui/core/Badge';
 import Tooltip from '@material-ui/core/Tooltip';
-import Avatar from '@material-ui/core/Avatar';
-import { deepPurple } from '@material-ui/core/colors';
 import Typography from '@material-ui/core/Typography';
 
-import { useSelector } from 'react-redux';
+import Avatar from '../../widgets/Avatar';
 
 const StyledBadge = withStyles((theme: Theme) =>
     createStyles({
@@ -43,11 +42,6 @@ const useStyles = makeStyles((theme: Theme) =>
         icon: {
             color: '#9e9e9e',
             fontSize: 30
-        },
-        avatar: {
-            width: 30,
-            height: 30,
-            fontSize: 14
         }
     })
 );
@@ -57,16 +51,11 @@ interface IHeaderProps {
 };
 
 const Header: React.FC<IHeaderProps> = props => {
-    const theme = useTheme();
     const classes = useStyles();
 
     const user = useSelector((state: any) => state.app.user);
 
     if (!user) return null;
-
-    const getInitials = () => {
-        return (user.firstName.charAt(0) + user.lastName.charAt(0)).toUpperCase();
-    };
 
     return (
         <AppBar id="Header" position="static" className={classes.appBar}>
@@ -83,14 +72,11 @@ const Header: React.FC<IHeaderProps> = props => {
                 <Tooltip title="Account">
                     <IconButton>
                         <Avatar
-                            className={classes.avatar}
-                            style={{
-                                backgroundColor: user.color ? user.color : deepPurple[500],
-                                color: theme.palette.getContrastText(user.color ? user.color : deepPurple[500])
-                            }}
-                        >
-                            {getInitials()}
-                        </Avatar>
+                            size="sm"
+                            firstName={user.firstName}
+                            lastName={user.lastName}
+                            color={user.color}
+                        />
                     </IconButton>
                 </Tooltip>
             </Toolbar>
