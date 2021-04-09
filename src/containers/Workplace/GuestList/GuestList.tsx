e767a22cb43154e65083f7230862709f3056e9f4
Vincent <vincentbenjaminpatoc@gmail.com>
2021-04-09 16:15:32 +0800

    Implement removing of Guest in GuestList.

--- src/containers/Workplace/GuestList/GuestList.tsx
+++ src/containers/Workplace/GuestList/GuestList.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, connect } from 'react-redux';
 
 import { makeStyles, createStyles, Theme } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
+import Popover from '@material-ui/core/Popover';
+import ListItem from '@material-ui/core/ListItem';
 
 import Avatar from '../../../widgets/Avatar';
 
 import Guest from '../../../models/types/Guest';
 import Invitation from '../../../models/types/Invitation';
 
+import * as actions from '../../../store/actions';
+
 interface IGuestListProps {
-    boardID?: string
+    boardID?: string,
+    deleteInvitation: (arg1: string) => Promise<any>,
 }
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -23,7 +28,7 @@ const useStyles = makeStyles((theme: Theme) =>
             minWidth: 30,
             minHeight: 32,
             margin: 0,
-            marginLeft: 4,
+            marginLeft: 5,
             borderRadius: '50%',
             boxShadow: 'none',
             '&:hover': {
@@ -36,6 +41,9 @@ const useStyles = makeStyles((theme: Theme) =>
 const GuestList: React.FC<IGuestListProps> = props => {
     const classes = useStyles();
 
+    const [selectedGuest, setSelectedGuest] = React.useState<Guest>();
+    const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+
     const guests: Guest[] = useSelector((state: any) => state.guest.guests);
     const invitations: Invitation[] = useSelector((state: any) => state.invitation.invitations);
 
@@ -46,13 +54,52 @@ const GuestList: React.FC<IGuestListProps> = props => {
         })
     }
 
+    const handleRemove = () => {
+        setAnchorEl(null);
+        
+        const invitation = invitations.find(i => i.guestID === selectedGuest?.id && i.boardID === props.boardID);
+
+        if (!invitation) return;
+
+        props.deleteInvitation(invitation.id);
+    };
+
     if (!props.boardID) return null;
     
     return (
         <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+            <Popover
+                id="board-selector-popup"
+                open={Boolean(anchorEl)}
+                anchorEl={anchorEl}
+                onClose={() => setAnchorEl(null)}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+                style={{marginTop: 5}}
+            >
+                <ListItem
+                    button
+                    onClick={handleRemove}
+                >
+                    Remove
+                </ListItem>
+            </Popover>
             {
                 filterGuests().map(guest =>
-                    <div key={"guest-list-" + guest.id} style={{marginLeft: 4}}>
+                    <Button
+                        key={"guest-list-" + guest.id}
+                        className={classes.button}
+                        onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
+                            setAnchorEl(event.currentTarget);
+                            setSelectedGuest(guest);
+                        }}
+                    >
                         <Avatar
                             size={32}
                             initials={
@@ -62,7 +109,7 @@ const GuestList: React.FC<IGuestListProps> = props => {
                             
                             color={guest.color}
                         />
-                    </div>
+                    </Button>
                 )
             }
             <Button className={classes.button} variant="contained">
@@ -72,4 +119,10 @@ const GuestList: React.FC<IGuestListProps> = props => {
     );
 };
 
-export default GuestList;
+const mapDispatchToProps = (dispatch: any) => {
+    return {
+        deleteInvitation: (id: string) => dispatch(actions.deleteInvitation(id))
+    }
+};
+
+export default connect(null, mapDispatchToProps)(GuestList);
