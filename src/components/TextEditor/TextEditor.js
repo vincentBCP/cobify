edacdc7579e2d32a36551cf4ee26082a37e5fdf9
Vincent <vincentbenjaminpatoc@gmail.com>
2021-04-11 22:29:15 +0800

    Create image upload adapter for CKEditor.

--- src/components/TextEditor/TextEditor.js
+++ src/components/TextEditor/TextEditor.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import MyUploadAdapterPlugin from './MyUploadAdapterPlugin';
 
 import './TextEditor.scss';
 
@@ -13,12 +14,14 @@ const TextEditor = props => {
                 editor={ ClassicEditor }
                 data=""
                 config={{
-                    toolbar: [ 'bold', 'italic', 'bulletedList', 'numberedList', 'blockQuote' ]
+                    extraPlugins: [ MyUploadAdapterPlugin ],
+                    toolbar: [ 'bold', 'italic', 'bulletedList', 'numberedList', 'blockQuote', 'imageUpload' ]
                 }}
                 onReady={ editor => {
                     // You can store the "editor" and use when it is needed.
                     //console.log( 'Editor is ready to use!', editor );
                     // console.log(Array.from( editor.ui.componentFactory.names() ));
+                    //console.log(editor.plugins);
                 } }
                 onChange={ ( event, editor ) => {
                     //const data = editor.getData();
