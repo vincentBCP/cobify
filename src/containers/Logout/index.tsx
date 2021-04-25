b595934489a38c9b10ed6cc22b50a31cc0dd45c6
Vincent <vincentbenjaminpatoc@gmail.com>
2021-04-25 19:35:48 +0800

    Remove app localStorage values upon logging out.

--- src/containers/Logout/index.tsx
+++ src/containers/Logout/index.tsx
@@ -4,6 +4,9 @@ import firebase from '../../firebase';
 
 const Logout: React.FC = props => {
     useEffect(() => {
+        localStorage.removeItem("refreshToken");
+        localStorage.removeItem("account");
+        
         firebase.auth().signOut();
         window.location.reload();
     }, []);
