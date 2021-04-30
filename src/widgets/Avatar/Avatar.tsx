fddb74978c01402d488d73d10ede68dc5113ad6f
Vincent <vincentbenjaminpatoc@gmail.com>
2021-04-30 21:35:42 +0800

    Tweak Avatar.

--- src/widgets/Avatar/Avatar.tsx
+++ src/widgets/Avatar/Avatar.tsx
@@ -17,13 +17,16 @@ interface IAvatarProps {
 const ProfileAvatar: React.FC<IAvatarProps> = props => {
     const theme = useTheme();
 
+    let backgroundColor = props.account.profilePicture ? "transparent" : props.account.color;
+
     return (
         <Avatar
             style={{
                 width: props.size,
                 height: props.size,
                 fontSize: props.size / 2,
-                backgroundColor: props.account.color ? props.account.color : deepPurple[500],
+                // backgroundColor: props.account.color ? props.account.color : deepPurple[500],
+                backgroundColor: backgroundColor,
                 color: theme.palette.getContrastText(props.account.color ? props.account.color : deepPurple[500])
             }}
             src={props.account.profilePicture ? props.account.profilePicture.downloadURL : ""}
